0915e9093387ec7a49a494b7f11e37c75d37ac35
Üllar Seerme <yates@protonmail.ch>
2023-09-20 16:05:09 +0300

    feat(segments.py): add ability to display exit node
    gearing up to refactor, add tests, and release

--- powerline_tailscale/segments.py
+++ powerline_tailscale/segments.py
@@ -7,26 +7,56 @@ from powerline.theme import requires_segment_info
 
 @requires_segment_info
 class TailscaleSegment(Segment):
-    def __call__(self, pl, segment_info, show_account: bool = True, **kwargs):
+    def __call__(self, pl, segment_info, show_exit_node: bool = True, **kwargs):
         try:
             if segment_info["environ"].get("POWERLINE_TAILSCALE") == "0":
                 return
         except TypeError:
             return
 
-        return self.build_segments(show_account)
+        return self.build_segments(show_exit_node)
 
-    def build_segments(self, show_account: bool = True):
+    def build_segments(self, show_exit_node: bool):
         segments = []
 
-        if show_account:
-            base = self.get_base_command()
-            profile = self.execute(base + ["http://local-tailscaled.sock/localapi/v0/prefs"])
-            d = json.loads(profile)
+        base = self.get_base_command()
+        profile = self.execute(base + ["http://local-tailscaled.sock/localapi/v0/prefs"])
+        d = json.loads(profile)
+        profile_name = d.get("ProfileName", None)
+        config = d.get("Config", None)
+
+        if profile_name is None and config is not None:
+            contents = "(ts) default"
+        elif profile_name is None and config is None:
+            contents = "(ts) logged out"
+        else:
+            contents = f"(ts) {profile_name}"
+
+        segments.append(
+            {
+                "contents": contents,
+                "highlight_groups": ["tailscale"],
+                "divider_highlight_group": "tailscale:divider",
+            }
+        )
+
+        if show_exit_node:
+            contents = "no exit node"
+            exit_node_ips = None
+
+            status = self.execute(base + ["http://local-tailscaled.sock/localapi/v0/status"])
+            d = json.loads(status)
+            exit_node = d.get("ExitNodeStatus", None)
+
+            if exit_node is not None:
+                exit_node_ips = exit_node.get("TailscaleIPs", None)
+            if exit_node_ips is not None:
+                contents = exit_node_ips[0]
+
             segments.append(
                 {
-                    "contents": f"(ts) {d['ProfileName']}",
-                    "highlight_groups": ["tailscale"],
+                    "contents": contents,
+                    "highlight_groups": ["tailscale_exitnode"],
                     "divider_highlight_group": "tailscale:divider",
                 }
             )
