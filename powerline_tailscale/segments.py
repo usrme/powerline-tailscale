964170d2ecf835c5690205c20d6e8456279573f1
Üllar Seerme <yates@protonmail.ch>
2023-09-20 20:59:22 +0300

    refactor(segments.py): differentiate between showing exit node status and IP

--- powerline_tailscale/segments.py
+++ powerline_tailscale/segments.py
@@ -1,62 +1,99 @@
 import json
 from subprocess import PIPE, Popen
+from typing import Union
 
 from powerline.segments import Segment, with_docstring
 from powerline.theme import requires_segment_info
 
+SEGMENT_PREFIX = "(ts)"
+
 
 @requires_segment_info
 class TailscaleSegment(Segment):
-    def __call__(self, pl, segment_info, show_exit_node: bool = True, **kwargs):
+    def __init__(self):
+        self.exit_node_ips: Union[list[str], None] = None
+
+    def __call__(
+        self,
+        segment_info,
+        show_profile_name: bool = True,
+        show_exit_node_status: bool = True,
+        show_exit_node: bool = False,
+        **kwargs,
+    ):
         try:
             if segment_info["environ"].get("POWERLINE_TAILSCALE") == "0":
                 return
         except TypeError:
             return
 
-        return self.build_segments(show_exit_node)
+        if show_exit_node_status or show_exit_node:
+            self.exit_node_ips = self._get_exit_node_ips()
 
-    def build_segments(self, show_exit_node: bool):
+        return self.build_segments(show_profile_name, show_exit_node_status, show_exit_node)
+
+    def build_segments(self, show_profile_name: bool, show_exit_node_status: bool, show_exit_node: bool):
         segments = []
 
-        base = self.get_base_command()
-        profile = self.execute(base + ["http://local-tailscaled.sock/localapi/v0/prefs"])
-        d = json.loads(profile)
-        profile_name = d.get("ProfileName", None)
-        config = d.get("Config", None)
+        if show_profile_name:
+            base = self.get_base_command()
+            profile = self.execute(base + ["http://local-tailscaled.sock/localapi/v0/prefs"])
+            d = json.loads(profile)
+            profile_name = d.get("ProfileName", None)
+            config = d.get("Config", None)
 
-        if profile_name is None and config is not None:
-            contents = "(ts) default"
-        elif profile_name is None and config is None:
-            contents = "(ts) logged out"
-        else:
-            contents = f"(ts) {profile_name}"
+            if profile_name is None and config is not None:
+                contents = "default"
+            elif profile_name is None and config is None:
+                contents = "logged out"
+            else:
+                contents = profile_name
 
-        segments.append(
-            {
-                "contents": contents,
-                "highlight_groups": ["tailscale"],
-                "divider_highlight_group": "tailscale:divider",
-            }
-        )
+            segments.append(
+                {
+                    "contents": f"{SEGMENT_PREFIX} {contents}",
+                    "highlight_groups": ["tailscale"],
+                    "divider_highlight_group": "tailscale:divider",
+                }
+            )
 
-        if show_exit_node:
-            contents = "no exit node"
-            exit_node_ips = None
+        if show_exit_node_status:
+            highlight_groups = ["tailscale_exitnode"]
+            contents = "exit node (n)"
 
-            status = self.execute(base + ["http://local-tailscaled.sock/localapi/v0/status"])
-            d = json.loads(status)
-            exit_node = d.get("ExitNodeStatus", None)
+            if self.exit_node_ips is not None:
+                contents = "exit node (y)"
 
-            if exit_node is not None:
-                exit_node_ips = exit_node.get("TailscaleIPs", None)
-            if exit_node_ips is not None:
-                contents = exit_node_ips[0]
+            if not show_profile_name and show_exit_node_status:
+                contents = f"{SEGMENT_PREFIX} {contents}"
+                highlight_groups = ["tailscale"]
 
             segments.append(
                 {
                     "contents": contents,
-                    "highlight_groups": ["tailscale_exitnode"],
+                    "highlight_groups": highlight_groups,
+                    "divider_highlight_group": "tailscale:divider",
+                }
+            )
+
+        # Exit node's value can only be shown if 'show_exit_node_status' is False
+        if show_exit_node and not show_exit_node_status:
+            highlight_groups = ["tailscale_exitnode"]
+            contents = "exit node (n)"
+
+            if self.exit_node_ips is not None:
+                # If both an IPv4 and IPv6 address exist, it should get the first
+                # which is usually IPv4
+                contents = self.exit_node_ips[0]
+
+            if not show_profile_name and show_exit_node:
+                contents = f"{SEGMENT_PREFIX} {contents}"
+                highlight_groups = ["tailscale"]
+
+            segments.append(
+                {
+                    "contents": contents,
+                    "highlight_groups": highlight_groups,
                     "divider_highlight_group": "tailscale:divider",
                 }
             )
@@ -70,6 +107,22 @@ class TailscaleSegment(Segment):
 
     def get_base_command(self):
         return ["curl", "--unix-socket", "/var/run/tailscale/tailscaled.sock"]
+
+    # TODO: This might not need to be an instance method, but rather just a function
+    # that reads a dictionary '{"ExitNodeStatus": {"TailscaleIPs": ["abc", "xyz"]}}'
+    def _get_exit_node_ips(self) -> Union[list[str], None]:
+        base = self.get_base_command()
+        status = self.execute(base + ["http://local-tailscaled.sock/localapi/v0/status"])
+        d = json.loads(status)
+        exit_node_status = d.get("ExitNodeStatus", None)
+        if exit_node_status is None:
+            return None
+
+        exit_node_ips = exit_node_status.get("TailscaleIPs", None)
+        if exit_node_ips is None:
+            return None
+
+        return exit_node_ips
 
 
 tailscale = with_docstring(TailscaleSegment(), """Return the status of Tailscale.""")
