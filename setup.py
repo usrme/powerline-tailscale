4592dd1484fa96d507e93c625867ca5521dd14b9
Üllar Seerme <yates@protonmail.ch>
2023-09-21 20:26:09 +0300

    build(setup.py): bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="powerline-tailscale",
     description="A Powerline segment for showing the status of Tailscale",
-    version="0.0.9",
+    version="0.0.10",
     keywords="powerline tailscale",
     license="MIT",
     author="Üllar Seerme",
